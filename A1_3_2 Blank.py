9dcca42c659c7e494c8b093ada8eed431a7e57e7
Drayzdin <57916828+Dt3202@users.noreply.github.com>
2019-12-02 13:23:46 -0600

    A function that takes a number and changes it to something else

--- A1_3_2 Blank.py
+++ A1_3_2 Blank.py
@@ -1,6 +1,13 @@
 """
-A1_3_2 #Replace With Your Last Name.py
-Programmer:
+A1_3_2 Thompson.py
+Programmer: Drayzdin
 """
 
 #Place Your Code Below
+
+guess = 13
+def hiLo_game(guess, change):
+    change = guess ** 2 + guess
+    return change / 7
+change = hiLo_game(guess, change)
+print(change)
