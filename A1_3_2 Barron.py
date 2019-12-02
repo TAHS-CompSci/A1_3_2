2e941f66e61dc11fb9e29fd06913a0fecb2f95db
KB3180 <57916885+KB3180@users.noreply.github.com>
2019-12-02 13:30:04 -0600

    We worked on Activity 1.3.2
    We made a Hilo Game

---
+++ A1_3_2 Barron.py
@@ -0,0 +1,13 @@
+"""
+A1_3_2 #Replace With Your Last Name.py
+Programmer:
+"""
+
+#Place Your Code Below 
+
+guess = 20 
+def hiLo_game(guess, change):
+    guess = change * 2 + 5 
+    return change / 7 
+change = hiLo_game(guess, change)
+print(change) 
