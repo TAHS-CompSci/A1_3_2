8bd81e83d49d3ba4a7e062383912161bec965e75
GC3018 <57916732+GC3018@users.noreply.github.com>
2019-12-03 13:21:04 -0600

    Program we have to submit
    contains if, else, if else, less than equill to, & greater than

---
+++ A1_3_2 campbell.py
@@ -0,0 +1,18 @@
+"""
+A1_3_2 #Replace With Your Last Name.py
+Programmer:
+"""
+
+#Place Your Code Below
+
+def hilo_game(guess):
+    guess=68
+    if:
+        guess>68
+        print (to high)
+    elif:
+        guess<42
+        print (to low)    
+    else: 
+        guess=42
+        print (correct)
